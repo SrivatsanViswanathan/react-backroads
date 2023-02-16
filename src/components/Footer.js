6ec47da02e45ae23f36b68a74db7d690d323de34
Srivatsan Viswanathan <sriv.viswanathan@gmail.com>
2023-02-15 22:37:51 -0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import { socialLinks } from "../data";
+import PageLinks from "./PageLinks";
+import SocialLink from "./SocialLink";
+
+const Footer = () => {
+  return (
+    <footer className='section footer'>
+      <PageLinks parentClass='footer-links' itemClass='footer-link'></PageLinks>
+      {/* <ul className='footer-links'>
+        {pageLinks.map((link) => {
+          const { id, href, text } = link;
+          return (
+            <li key={id}>
+              <a href={href} className='footer-link'>
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul> */}
+      <ul className='footer-icons'>
+        {socialLinks.map((link) => {
+          return (
+            <SocialLink
+              key={link.id}
+              {...link}
+              itemClass='footer-icon'
+            ></SocialLink>
+          );
+        })}
+      </ul>
+      <p className='copyright'>
+        copyright &copy; Backroads travel tours company
+        <span id='date'>{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/components/Services.js
@@ -0,0 +1,22 @@
+import Title from "./Title";
+import { services } from "../data";
+import Service from "./Service";
+const Services = () => {
+  return (
+    <section className='section services' id='services'>
+      <Title title='our' subtitle='services'></Title>
+      <div className='section-center services-center'>
+        {services.map((service) => {
+          return (
+            <Service
+              key={service.id}
+              {...service}
+              itemClass='nav-icon'
+            ></Service>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+export default Services;
